ad111c4fc53113ccf36c03735970eb1b5e4a7276
Antonio <antonio.agj@gmail.com>
2021-03-23 11:51:53 +0100

    minor changes...

--- org.module.eer.project/bundles/org.module.eer.jenetics.linked.list/src/org/module/eer/jenetics/linked/list/chromosome/crossover/GreedyPartitionCrossover.java
+++ org.module.eer.project/bundles/org.module.eer.jenetics.linked.list/src/org/module/eer/jenetics/linked/list/chromosome/crossover/GreedyPartitionCrossover.java
@@ -19,6 +19,7 @@ public class GreedyPartitionCrossover implements Alterer<IntegerGene, Vec<double
 	public AltererResult<IntegerGene, Vec<double[]>> alter(Seq<Phenotype<IntegerGene, Vec<double[]>>> population, long generation) {
 		AltererResult<IntegerGene, Vec<double[]>> result = null;
 		if (population.size() >= 2) {
+			//Create Cluster List<List<Integer>> for each parent
 			//Perform 
 			System.out.println("Perform Suff");
 		} else {
